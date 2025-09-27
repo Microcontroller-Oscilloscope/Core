ec00ff14ad075e284fded428052064ba1231bbcb
Camren Chraplak <cchraplak@wpi.edu>
2025-09-27 13:33:00 -0400

    Simplified Esp32 timer flags

--- src/boards/esp32/board_esp32.h
+++ src/boards/esp32/board_esp32.h
@@ -80,15 +80,12 @@
 	typedef void (*hard_timer_function_ptr_t) (void); // timer callback function pointer
 
 	// timer references
-	#define HARD_TIMER0_REFERENCE hardTimer0Function // reference for timer 0 function
-	#define HARD_TIMER1_REFERENCE hardTimer1Function // reference for timer 1 function
-	#define HARD_TIMER2_REFERENCE hardTimer2Function // reference for timer 2 function
-	#define HARD_TIMER3_REFERENCE hardTimer3Function // reference for timer 3 function
+	#define HARD_TIMER_REFERENCE(id) hardTimerFunction##id // reference for timer #id
 
 	// timer functions
 
 	/**
-	 * Starter function for timer 0
+	 * Starter function for timer #id
 	 * 
 	 * APB_CLK = 80,000,000Hz
 	 * 
@@ -96,7 +93,9 @@
 	 * 
 	 * T_DER = timerTicks * (scalar / APB_CLK)
 	 * 
-	 * @note HARD_TIMER0_FUNCTION() {
+	 * @param id numeric value to append to function
+	 * 
+	 * @note HARD_TIMER_FUNCTION({id}) {
 	 * @note 	{contents}
 	 * @note 	HARD_TIMER_END();
 	 * @note }
@@ -104,64 +103,7 @@
 	 * @warning 64-bit counter
 	 * @warning 64-bit scalar
 	 */
-	#define HARD_TIMER0_FUNCTION() void IRAM_ATTR HARD_TIMER0_REFERENCE()
-
-	/**
-	 * Starter function for timer 1
-	 * 
-	 * APB_CLK = 80,000,000Hz
-	 * 
-	 * T_DER = desired time (s)
-	 * 
-	 * T_DER = timerTicks * (scalar / APB_CLK)
-	 * 
-	 * @note HARD_TIMER1_FUNCTION() {
-	 * @note 	{contents}
-	 * @note 	HARD_TIMER_END();
-	 * @note }
-	 * 
-	 * @warning 64-bit counter
-	 * @warning 64-bit scalar
-	 */
-	#define HARD_TIMER1_FUNCTION() void IRAM_ATTR HARD_TIMER1_REFERENCE()
-
-	/**
-	 * Starter function for timer 2
-	 * 
-	 * APB_CLK = 80,000,000Hz
-	 * 
-	 * T_DER = desired time (s)
-	 * 
-	 * T_DER = timerTicks * (scalar / APB_CLK)
-	 * 
-	 * @note HARD_TIMER0_FUNCTION() {
-	 * @note 	{contents}
-	 * @note 	HARD_TIMER_END();
-	 * @note }
-	 * 
-	 * @warning 64-bit counter
-	 * @warning 64-bit scalar
-	 */
-	#define HARD_TIMER2_FUNCTION() void IRAM_ATTR HARD_TIMER2_REFERENCE()
-
-	/**
-	 * Starter function for timer 3
-	 * 
-	 * APB_CLK = 80,000,000Hz
-	 * 
-	 * T_DER = desired time (s)
-	 * 
-	 * T_DER = timerTicks * (scalar / APB_CLK)
-	 * 
-	 * @note HARD_TIMER0_FUNCTION() {
-	 * @note 	{contents}
-	 * @note 	HARD_TIMER_END();
-	 * @note }
-	 * 
-	 * @warning 64-bit counter
-	 * @warning 64-bit scalar
-	 */
-	#define HARD_TIMER3_FUNCTION() void IRAM_ATTR HARD_TIMER3_REFERENCE()
+	#define HARD_TIMER_FUNCTION(id) void IRAM_ATTR hardTimerFunction##id ()
 
 	#define HARD_TIMER_END() // end of function for timers
 
@@ -170,8 +112,8 @@
 	****************************/
 
 	#define HARD_TIMER_LED HARD_TIMER0 // hardware timer for status LEDs
-	#define HARD_TIMER_LED_FUNCTION() HARD_TIMER0_FUNCTION() // starter function for status LED
-	#define HARD_TIMER_LED_REFERENCE HARD_TIMER0_REFERENCE // reference for status LED function
+	#define HARD_TIMER_LED_FUNCTION() HARD_TIMER_FUNCTION(HARD_TIMER_LED) // starter function for status LED
+	#define HARD_TIMER_LED_REFERENCE HARD_TIMER_REFERENCE(HARD_TIMER_LED) // reference for status LED function
 
 	#define HARD_TIMER_LED_SCALAR 80 // pre scalar for LED timer
 	#define HARD_TIMER_LED_TICK_MULTIPLIER 1000 // multiplier for timer ticks
